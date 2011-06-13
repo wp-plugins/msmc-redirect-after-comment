59b999fed577e7109fc2324b3df79133159ac2af
citricguy <citricguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 11:23:10 +0000

    fixing version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/msmc-redirect-after-comment/tags/2.1@396586 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/msmc_redirect.php
@@ -0,0 +1,140 @@
+<?php
+/*
+  Plugin Name: MSMC - Redirect After Comment
+  Plugin URI: http://www.1mauirealestate.com/tech-updates/wordpress-plugin-redirect-after-comment.html
+  Description: Redirects commenters to a predefined URL after clicking submit.
+  Author: Josh Sommers
+  Version: 2.1
+  Author URI: http://www.mainstreetmarketingcommunity.com
+ */
+
+/*  Josh Sommers  (Email : citricguy@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+if (!load_plugin_textdomain('msmc-redirect-after-comment', '/wp-content/languages/'))
+    load_plugin_textdomain('msmc-redirect-after-comment', '/wp-content/plugins/msmc-redirect-after-comment/');
+
+/* Installation and Removal */
+register_activation_hook(__FILE__, 'MSMC_redirect_install');
+register_deactivation_hook(__FILE__, 'MSMC_redirect_remove');
+
+function MSMC_redirect_install() {
+    add_option("MSMC_redirect_settings", array("redirect_to" => "http://" . $_SERVER['HTTP_HOST'], "enabled" => ''), '', 'yes');
+}
+
+function MSMC_redirect_remove() {
+    delete_option('MSMC_redirect_settings');
+}
+
+/* Redirect Code */
+$settings = get_option('MSMC_redirect_settings');
+if ($settings['enabled'] != '') {
+    add_action('comment_post_redirect', 'msmc_do_comment_redirect');
+}
+
+function msmc_do_comment_redirect() {
+    //echo "<pre>"; var_dump($GLOBALS); die;
+    $settings = get_option('MSMC_redirect_settings');
+    if (strtolower($settings['redirect_to']) == ('[last]')) {
+        return urldecode($_REQUEST["msmc_redirect_referrer"]);
+    }
+    return $settings['redirect_to'];
+}
+
+add_action('comment_form', 'msmc_insert_hidden_field');
+
+function msmc_insert_hidden_field() {
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
+            || $_SERVER['SERVER_PORT'] == 443) {
+
+        $msmc_protocol = 'https://';
+    } else {
+        $msmc_protocol = 'http://';
+    }
+    echo '<input type="hidden" name="msmc_redirect_referrer" value="' .  $msmc_protocol . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . '" />';
+}
+
+/* Administration */
+add_action('admin_menu', 'my_first_admin_menu');
+
+function my_first_admin_menu() {
+    add_options_page('MSMC Comment Redirect Options', 'Comment Redirect', 'manage_options', 'msmc-comment-redirect', 'msmc_comment_redirect_plugin_page');
+}
+
+function msmc_comment_redirect_plugin_page() {
+    $settings = get_option('MSMC_redirect_settings');
+
+    if ($_REQUEST['action']) {
+        // Defaults
+        $update_array = array("redirect_to" => "http://" . $_SERVER['HTTP_HOST'], "enabled" => '');
+        $do_update = TRUE;
+        // Enable Plugin
+        if (isset($_REQUEST['MSMC_redirect_enabled'])) {
+            $update_array['enabled'] = ' checked="checked" ';
+            $settings['enabled'] = ' checked="checked" ';
+        } else {
+            $settings['enabled'] = '';
+        }
+
+        // Set Redirect URL
+        if (isset($_REQUEST['MSMC_redirect_location'])) {
+            $_REQUEST['MSMC_redirect_location'] = strtolower($_REQUEST['MSMC_redirect_location']);
+            // Requires PHP 5.2.0 or newer
+            if ((filter_var($_REQUEST['MSMC_redirect_location'], FILTER_VALIDATE_URL)) || (strtolower($_REQUEST['MSMC_redirect_location']) == '[last]')) {
+                $update_array['redirect_to'] = $_REQUEST['MSMC_redirect_location'];
+                $settings['redirect_to'] = $_REQUEST['MSMC_redirect_location'];
+            } else {
+                $do_update = FALSE;
+                $settings['redirect_to'] = $_REQUEST['MSMC_redirect_location'];
+                $notice_warning = __("Please Fix The Errors Below", 'msmc-redirect-after-comment');
+                $result_notice = '<div style="height: 20px; width: 500px; background-color: #FF6699; padding: 10px; font-weight: bold;" >' . $notice_warning . '</div>';
+                $notice_validate = __("Please Enter A Valid URL", 'msmc-redirect-after-comment');
+                $show_url_warning = '<span style="color: red;">' . $notice_validate . '</span><br />';
+            }
+        }
+
+        // Update Settings
+        if ($do_update) {
+            update_option('MSMC_redirect_settings', $update_array);
+            $notice_success = __("Updated Settings Successfully", 'msmc-redirect-after-comment');
+            $result_notice = '<div style="height: 20px; width: 500px; background-color: #FFFBCC; padding: 10px; font-weight: bold;" >' . $notice_success . '</div>';
+        }
+    }
+    ?>
+    <div class="wrap">
+        <h2>MSMC - Redirect After Comment</h2>
+    <?php echo $result_notice; ?>
+        <form method="post" action="" id="feedflare-settings">
+            <br />
+            <input type="checkbox" name="MSMC_redirect_enabled" value="checked" <?php echo $settings['enabled']; ?> /> <strong><?php _e("Enable Plugin?", 'msmc-redirect-after-comment'); ?></strong><br /><br />
+            <strong><?php _e("Enter Redirect URL", 'msmc-redirect-after-comment'); ?>:</strong><br />
+            <span style="font-style: italic; color: #B2B2B2;"><?php _e("This is where users will be redirected after commenting.", 'msmc-redirect-after-comment'); ?></span><br />
+    <?php echo $show_url_warning; ?>
+            <input type="text" name="MSMC_redirect_location" size="60" id="MSMC_redirect_location"
+                   value="<?php echo $settings['redirect_to']; ?>" /><br />
+            <p><h2>Additional Usage Notes</h2>Enter URL's in using the standard complete format: <strong>http://www.example.com/example_extended</strong><br /><br />You can also enter <strong>[last]</strong> (include the square brackets) <br />if you wish to send commenters back to the page they originally commented from.</p>
+
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="MSMC_redirect_settings" />
+            <p>
+                <input type="submit" value="<?php _e('Save Changes', 'msmc-redirect-after-comment') ?>" />
+            </p>
+        </form>
+    </div>
+    <?php
+}
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,95 @@
+=== Plugin Name ===
+Contributors: citricguy
+Donate link: http://www.1mauirealestate.com/tech-updates/wordpress-plugin-redirect-after-comment.html
+Tags: comments, redirect, tellafriend, plugin
+Requires at least: 2.8
+Tested up to: 3.1.3
+Stable tag: 2.1
+
+Redirects commenters to a predefined URL after comment submission.
+
+== Description ==
+
+The "Redirect After Comment" plugin allows you to automatically redirect your users to a predefined page after they leave a comment on your WordPress blog.
+
+Usage is simple.  Once the plugin has been installed you will need to configure where your commenters should be redirected.
+
+In the WordPress control panel, on the left hand side of the admin panel look in the ‘settings’ section for the heading titled ‘Comment Redirect.’
+
+In the settings you will see two fields:
+
+The first field is the "Enable Plugin?" option.  The plugin is not functional until you check this box.  This allows you to quickly enable and disable this plugin while you’re working on the page you plan to send commenters.
+
+The second field is titled "Enter Redirect URL."  Here is where you will enter the full URL to where you want to send users once they have successfully submitted a comment on your blog.  For example, if you wanted to send all of your commenters to example.com you would simply enter http://www.example.com/ in the box.
+
+You can also enter "[last]" in the "Enter Redirect URL" box to send users back to the page they originally commented from.
+
+If you already have a page on your site that you would like to send users to, simply copy and paste that URL into the "Enter Redirect URL" field.
+
+Ideas for redirects:
+1. A "Tell-a-friend" page.
+2. A survey.
+3. Your Facebook FanPage/Profile
+4. A Sales Page or A Special Offer
+
+*Note: PHP 5.2.0 or greater required for URL verification.*
+
+== Installation ==
+
+1. Upload `msmc_redirect.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Configure settings in control panel.
+
+== Frequently Asked Questions ==
+
+= How can I help? =
+
+Contact me at citricguy@gmail.com
+
+== Screenshots ==
+
+1. Plugin Options
+
+== Changelog ==
+
+= 2.1.0  =
+* Fixed license
+* Added the ability to redirect users back to the page they originally commented from. (Using "[last]" in the URL field.)
+* Fixed HTTP vs HTTPS detection while using [last]
+* No longer force lower case url's (sorry IIS uers)
+* As always, please report any bugs directly to citricguy@gmail.com!  Mahalo!
+
+= 2.0.1  =
+* Readme.txt updates
+
+= 2.0  =
+* Added Internationalization Support
+
+= 1.1 =
+* License and Readme.txt edits.  No Feature Updates
+
+= 1.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 2.0 =
+* Still need language files.
+
+= 1.1 =
+* No feature changes. Update not required.
+
+= 1.0 =
+* Initial Release
+
+== Other Information ==
+
+The MSMC - Redirect After Comment plugin was developed by Josh Sommers, and is provided with out warranty under the GPLv3 License. More info and other plugins at: http://www.1mauirealestate.com/tech-updates/wordpress-plugin-redirect-after-comment.html
+
+Josh Sommers  (email : citricguy@gmail.com)
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
